cd3c22a6eed5b3f3aca13f47aab9c78d134335a6
Robin9800 <robin.costanzo@gmail.com>
2022-03-09 10:13:26 +0100

    fixing

--- exercise.js
+++ exercise.js
@@ -1,7 +1,20 @@
 const divSquares = document.querySelector("#squares");
 for(let squareIndex = 1; squareIndex < 101; squareIndex++){
-    const element = `<div id="square-${squareIndex}" class="square">${squareIndex}</div>`;
-    divSquares.innerHTML += element;
 
     
+    let message = squareIndex
+
+    if(squareIndex % 15 === 0){
+        message = 'fizzbuzz';
+    }
+        else if(squareIndex % 3 === 0){
+            message = 'fizz';
+    }
+        else if(squareIndex % 5 === 0){
+            message = 'fizzbuzz';
+    }
+
+    const element = `<div id="square-${message}" class="square">${message}</div>`;
+    divSquares.innerHTML += element;
+
 }
